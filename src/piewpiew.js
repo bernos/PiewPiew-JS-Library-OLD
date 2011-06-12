e3b04b07241ae7e1ade197a27254a8f3d651350f
Brendan McMahon <bernos@gmail.com>
2011-06-12 16:05:36 +1000

    Started on StringBundle and TemplateContext

--- src/piewpiew.js
+++ src/piewpiew.js
@@ -1,3 +1,6 @@
+/**
+ * The PiewPiew module
+ */
 var PiewPiew = (function() {
   
   /**
@@ -6,7 +9,14 @@ var PiewPiew = (function() {
   var idSequence = 1;
   
   /**
+   * Default value to use for locale if one is not provided
+   */
+  var _defaultLocale = 'defaultLocale';
+  
+  /**
    * Extends one object by adding properties from another object
+   * 
+   * @alias PiewPiew.extend
    * 
    * @param {Object} object1
    *  The object to extend
@@ -27,7 +37,107 @@ var PiewPiew = (function() {
     }
     
     return base;
-  }
+  };
+  
+  /**
+   * Creates a method delegate. Enables us to specify the scope a function 
+   * should be run in when assigning and binding handlers.
+   * 
+   * @alias PiewPiew.createDelegate
+   * @param {object} obj
+   * @param {function} method
+   */
+  var createDelegate = function(obj, method) {
+    return function() {
+      method.apply(obj, arguments);
+    };
+  }; 
+  
+  var StringBundle = (function(){
+    var _strings = {};
+    
+    return {
+      /**
+       * Add strings to the StringBundle
+       * 
+       * @param {object} strings
+       *  An object containing keys/strings to add to the StringBundle
+       * @return {object} 
+       *  A reference to the StringBundle. Useful for method chaining.
+       */
+      addStrings: function(strings, locale) {
+        if(!locale) {
+          locale = PiewPiew.locale;
+        }
+        
+        if(!_strings[locale]) {
+          _strings[locale] = {};
+        }
+        
+        _strings[locale] = PiewPiew.extend(_strings[locale], strings);
+        
+        return this;
+      },
+      
+      /**
+       * Retrieves a string from the StringBundle
+       * 
+       * @param {string} key
+       *  Key of the string to retrieve
+       * @param {string} defaultValue
+       *  Default value to be returned if no string is found
+       * @return {string}
+       *  Either the requested string, or the default provided
+       */
+      getString: function(key, defaultValue) {
+        if (_strings[PiewPiew.locale][key]) {
+          return _strings[PiewPiew.locale][key];
+        }
+        
+        if (defaultValue) {
+          return defaultValue;
+        }
+        
+        return key;
+      }
+    }
+  }());
+  
+  /**
+   * Creates a context object ready for rendering. TemplateContexts provide data
+   * and helper functions to templates. New helper functions can be added by
+   * calling PiewPiew.TemplateContext.addHelpers()
+   */
+  var TemplateContext = (function() {
+    var helpers = {
+      /**
+       * Helper function for retrieving strings from the string bundle
+       */
+      s:function() {
+        return function(key, render) {
+/**
+          var strings = PMQuizStrings;
+
+          if (strings[key]) {
+            return render(strings[key]);
+          }
+
+          return 'No copy defined for  ' + key;
+          */
+        }
+      }
+    };
+    
+    var templateContext = function(data) {
+      return PiewPiew.extend({}, helpers, data);
+    };
+    
+    templateContext.addHelpers = function(newHelpers) {
+      PiewPiew.extend(helpers, newHelpers);
+    };
+    
+    return templateContext;
+  }());
 
   /**
    * Creates a View instance
@@ -37,6 +147,7 @@ var PiewPiew = (function() {
    *  that will be created
    * @return {PiewPiew.View}
    *  A View object
+   * @constructor
    */
   var View = function(spec) {
     /**
@@ -44,29 +155,49 @@ var PiewPiew = (function() {
      */
     var defaults = {
            id: "View" + (idSequence++),
-      tagname: "div"
+      tagname: "div",
+      classes: [],
+       events: {}
     };
     
     /**
-     * Extend the base View object with the defaults and spec if provided, then
-     * return it.
+     * Internal initialiser. This gets called when the View is first created
+     * We are mainly concerned with setting up any required params that have
+     * not been defined in the default or spec objects, calling our 
+     * overridable init() function, and returning a reference to ourself
      */
-    return extend({
-      /**
-       * Internal initialiser. This gets called when the View is first created
-       * We are mainly concerned with setting up an required params that have
-       * not been defined in the default or spec objects, calling our 
-       * overridable init() function, and returning a reference to ourself
-       */
-      __init__: function() {
-        
-        if (!this.el) {
-          this.el = document.createElement(this.tagname);
-          this.el.setAttribute("id", this.id);
+    var _initView = function(view) {
+      // Set up the root DOM element for the view
+      if (!view.el) {
+        view.el = document.createElement(view.tagname);
+        view.el.setAttribute('id', view.id);
+        view.el.setAttribute('class', view.classes.join(' '));
+      } 
+      
+      // Initialise DOM event handlers. Handlers may have been declared as 
+      // either functions or strings. Strings will be interpreted the names of
+      // handlers defined in the view object, and delegate functions will be
+      // created to be bound to each DOM event.
+      for(var selector in view.events) {
+        for(var eventName in view.events[selector]) {
+          var handler = view.events[selector][eventName];
+          if (typeof handler == 'string' && typeof view[handler] == 'function') {
+            view.events[selector][eventName] = PiewPiew.createDelegate(view, view[handler]);
+          }
         }
-        
-        return this;
-      },
+        $(view.el).delegate(selector, view.events[selector]);    
+      }
+      
+      view.init();
+      
+      return view;
+    }
+    
+    /**
+     * Extends the base View object with the defaults and spec if provided, then
+     * initialises the View and returns it.
+     */
+    return _initView(extend({
       
       /**
        * Initialise the View. Extended Views should provide their own 
@@ -85,29 +216,49 @@ var PiewPiew = (function() {
         return this;  
       }
       
-    }, defaults, spec || {}).__init__();
+    }, defaults, spec || {}));
   };
 
   /**
    * Return the PiewPiew module
    */
   return {
-    extend: extend,
-      View: View
+              extend: extend,
+      createDelegate: createDelegate,
+              locale: _defaultLocale,
+        StringBundle: StringBundle,
+     TemplateContext: TemplateContext,
+                View: View
   };
 }());
 
+
+
 var MyViewClass = function(spec) {
-  var view = PiewPiew.View(PiewPiew.extend({
+  var defaults = {
+    classes:['MyViewClass']
+  };
+  
+  var view = {
     type:"MyViewClass",
     
     render: function() {
-      this.el.innerHTML = "HELLO WORLD";
+      this.el.innerHTML = "HELLO WORLD <a class='button' href='#'>click</a>";
       return this;
+    },
+    
+    events: {
+      'a.button':{
+        'click': 'handleButtonClick'
+      }
+    },
+    
+    handleButtonClick: function(e) {
+      console.log("click", e);
     }
-  }, spec));
+  };
 
-  return view;
+  return PiewPiew.View(PiewPiew.extend(view, defaults, spec));
 }
 
 var MyViewSubClass = function(spec) {
